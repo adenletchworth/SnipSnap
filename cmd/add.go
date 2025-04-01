e45cbe5c7174d7fd0f9a5f4b75eb0a2c868e09a2
adenletchworth <adenrletchworth@gmail.com>
2025-04-01 15:20:10 -0700

    feat(cmd): implement 'add' CLI command for inserting new snippets

--- cmd/add.go
+++ cmd/add.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"SnipSnap/db"
+	"SnipSnap/model"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,10 +17,39 @@ var addCmd = &cobra.Command{
 You can provide a snippet inline and use flags to add metadata
 like programming language or description.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Add Command")
+		title, _ := cmd.Flags().GetString("title")
+		tags, _ := cmd.Flags().GetString("tags")
+		codeContent, _ := cmd.Flags().GetString("code")
+
+		snippet := model.Snippet{
+			Title:   title,
+			Tags:    strings.Split(tags, ","),
+			Content: codeContent,
+		}
+
+		store, err := db.NewSnippetStore("./snippets.db")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer store.Close()
+
+		id, err := store.InsertSnippet(snippet)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Snippet added with ID %d\n", id)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
+
+	// Flags
+	addCmd.Flags().String("title", "", "Title of the snippet")
+	addCmd.Flags().String("tags", "", "Comma-separated list of tags")
+	addCmd.Flags().String("code", "", "Code content of the snippet")
 }
